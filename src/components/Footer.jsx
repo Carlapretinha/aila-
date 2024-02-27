42ad76a4395d12eed29c509cf4f7f9a4add35267
Carlapretinha <128608213+Carlapretinha@users.noreply.github.com>
2024-02-27 11:06:09 -0300

    aula1

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p>Toodos os direitos reservdos - 2024</p>
+        <p>Desenvolvimemto por Carla</p>
+    </footer>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Logo from '../assets/Usher.webp.png'
+
+const Header = () => {
+  return (
+    <header>
+      <img src={Logo} alt=" logo imagem Usher e Alicia" />
+      <h1>Exercício 2 - React</h1>
+    </header>
+  )
+}
+
+export default Header
